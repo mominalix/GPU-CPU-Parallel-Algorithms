e2a3c67ce4bd247b36abdb74d1088197261c76a2
mominalix <mominalix@gmail.com>
2023-08-27 23:03:16 +0500

    new file:   Performance Optimization/Sparse Matrix-Vector Multiplication (SpMV)/SpMP_pyopencl.py

---
+++ Performance Optimization/Sparse Matrix-Vector Multiplication (SpMV)/SpMP_pyopencl.py
@@ -0,0 +1,51 @@
+import pyopencl as cl
+import numpy as np
+import scipy.sparse as sp
+
+# Set up OpenCL context and command queue
+platform = cl.get_platforms()[0]
+device = platform.get_devices()[0]
+context = cl.Context([device])
+queue = cl.CommandQueue(context)
+
+# Define a sparse matrix in Compressed Sparse Column (CSC) format
+# You can load a real sparse matrix or create a synthetic one
+rows = np.array([0, 1, 2, 2, 3])
+cols = np.array([0, 1, 2, 3, 3])
+data = np.array([1, 2, 3, 4, 5])
+matrix = sp.csc_matrix((data, (rows, cols)))
+
+# Define a vector for multiplication
+vector = np.array([1, 2, 3, 4])
+
+# Create OpenCL buffers for matrix and vector
+matrix_buf = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=matrix.data)
+row_indices_buf = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=matrix.indptr)
+column_indices_buf = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=matrix.indices)
+vector_buf = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=vector)
+
+# Load and compile OpenCL kernel
+kernel_code = """
+__kernel void spmv_kernel(__global const float* matrix, __global const int* row_indices,
+                          __global const int* column_indices, __global const float* vector,
+                          __global float* result, int num_rows)
+{
+    int gid = get_global_id(0);
+    if (gid < num_rows)
+    {
+        float sum = 0.0f;
+        for (int j = row_indices[gid]; j < row_indices[gid + 1]; ++j)
+            sum += matrix[j] * vector[column_indices[j]];
+        result[gid] = sum;
+    }
+}
+"""
+program = cl.Program(context, kernel_code).build()
+
+# Execute the kernel
+result = np.zeros(matrix.shape[0], dtype=np.float32)
+program.spmv_kernel(queue, result.shape, None, matrix_buf, row_indices_buf,
+                    column_indices_buf, vector_buf, result_buf, np.int32(matrix.shape[0]))
+
+# Print the result
+print("Result of SpMV using OpenCL:", result)
